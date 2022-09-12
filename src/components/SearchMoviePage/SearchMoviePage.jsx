14a32b65068726a1c068d13f9848152547f74130
MateuszBachor <mateuszbachor3@gmail.com>
2022-09-12 18:44:45 +0200

    ad

--- src/components/SearchMoviePage/SearchMoviePage.jsx
+++ src/components/SearchMoviePage/SearchMoviePage.jsx
@@ -9,24 +9,24 @@ const SearchMoviePage = () => {
   const [query, setQuery] = useState([]);
   const [arrayMovies, setArrayMovies] = useState([]);
   useEffect(() => {
+    const showMoviesArray = async () => {
+      const fetchMovies = async query => {
+        try {
+          const response = await axios.get(
+            `https://api.themoviedb.org/3/search/movie?api_key=168c87e340b321f808dc48486f6cc30b&query=${query}`
+          );
+
+          return response.data;
+        } catch (error) {
+          console.log('Error: ' + error);
+        }
+      };
+      const response = await fetchMovies(query);
+      console.log(response.results);
+      setArrayMovies(() => response.results);
+    };
     showMoviesArray();
   }, [query]);
-  const showMoviesArray = async () => {
-    const fetchMovies = async query => {
-      try {
-        const response = await axios.get(
-          `https://api.themoviedb.org/3/search/movie?api_key=168c87e340b321f808dc48486f6cc30b&query=${query}`
-        );
-
-        return response.data;
-      } catch (error) {
-        console.log('Error: ' + error);
-      }
-    };
-    const response = await fetchMovies(query);
-    console.log(response.results);
-    setArrayMovies(() => response.results);
-  };
 
   const handleSubmit = evt => {
     evt.preventDefault();
