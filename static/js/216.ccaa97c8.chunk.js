a92887f4379dc234e8eef03d859ee7f29817c53a
MateuszBachor <92057655+MateuszBachor@users.noreply.github.com>
2022-09-12 16:45:32 +0000

    Deploying to gh-pages from @ MateuszBachor/react-homework-5@14a32b65068726a1c068d13f9848152547f74130 🚀

---
+++ static/js/216.ccaa97c8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_5=self.webpackChunkreact_homework_5||[]).push([[216],{7216:function(e,r,o){o.r(r);o(2791);var t=o(184);r.default=function(){return(0,t.jsx)("img",{style:{width:"100%",height:"100%"},src:"https://img.freepik.com/premium-vector/modern-minimal-found-error-icon-oops-page-found-404-error-page-found-with-concept_599740-716.jpg?w=2000",alt:""})}}}]);
+//# sourceMappingURL=216.ccaa97c8.chunk.js.map
