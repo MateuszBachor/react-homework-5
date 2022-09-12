941cc879135df955c2feffc8de38498c763dacc7
MateuszBachor <mateuszbachor3@gmail.com>
2022-09-12 18:39:07 +0200

    .

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="react-homework-5">
+    <BrowserRouter basename="https://mateuszbachor.github.io/react-homework-5/">
     <App />
     </BrowserRouter>
   </React.StrictMode>
