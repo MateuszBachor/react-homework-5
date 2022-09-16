487cf441ba2158fefa87d9a7e7127b56f7b37cef
MateuszBachor <mateuszbachor3@gmail.com>
2022-09-16 08:32:08 +0200

    asd

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="react-homework-5">
+    <BrowserRouter >
     <App />
     </BrowserRouter>
   </React.StrictMode>
